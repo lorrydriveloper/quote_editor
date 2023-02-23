2fdf580ee03d4323a8d1f418fd480108ed2d0840
lorrydriveloper <lorrydriveloper@gmail.com>
2023-02-23 08:28:53 +0000

    create lineItemDates and artifacts

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -9,6 +9,7 @@ class Quote < ApplicationRecord
   scope :ordered, -> { order(created_at: :desc) }
 
   belongs_to :company
+  has_many :line_item_dates, dependent: :destroy
 
   validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :quotes
+  resources :quotes do
+    resources :line_item_dates, expect: %i[index show]
+  end
 end
