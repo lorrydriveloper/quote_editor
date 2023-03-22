c610c064e1981123e3a4fe142b4160a626e9ccba
lorrydriveloper <lorrydriveloper@gmail.com>
2023-03-22 08:32:59 +0000

    add more test

--- test/system/line_items_test.rb
+++ test/system/line_items_test.rb
@@ -6,28 +6,57 @@ class LineItemsTest < ApplicationSystemTestCase
   setup do
     login_as users(:accountant)
     @quote = quotes(:second)
+
+    visit quote_path(@quote)
   end
 
   test 'Create a New Line Item' do
-    visit quote_path(@quote)
     create_new_item(DateTime.current)
     assert_selector 'h1', text: @quote.name
     assert_text I18n.l(@quote.line_item_dates.last.date, format: :long)
   end
 
+  test 'Updating a Line Item' do
+    create_new_item(DateTime.current)
+    assert_selector 'h1', text: @quote.name
+    assert_text I18n.l(@quote.line_item_dates.last.date, format: :long)
+    target = @quote.line_item_dates.first
+
+    within id: dom_id(target) do
+      click_on 'Edit'
+    end
+    assert_selector 'h1', text: @quote.name
+
+    fill_in 'Date', with: Date.current + 1.day
+    click_on 'Update Line item date'
+
+    assert_text I18n.l(Date.current + 1.day, format: :long)
+  end
+
+  test 'Destroy a Line Item' do
+    create_new_item(DateTime.current)
+    sleep 0.5
+    target = @quote.line_item_dates.last
+    assert_selector 'h1', text: @quote.name
+    assert_text I18n.l(target.date, format: :long)
+    within id: dom_id(target) do
+      click_on 'Delete'
+    end
+    page.driver.browser.switch_to.alert.accept
+    assert_selector 'h1', text: @quote.name
+    assert_no_text I18n.l(Date.current, format: :long)
+  end
+
   test 'Add new item after prev date' do
-    visit quote_path(@quote)
     create_new_item(DateTime.current)
     create_new_item(DateTime.current + 1.day)
 
     assert_selector 'h1', text: @quote.name
-    prev_date = @quote.line_item_dates.last.prev_date
-    element = page.find("#line_item_date_#{prev_date.to_param}")
+    element = page.find("#line_item_date_#{@quote.line_item_dates.last.prev_date.to_param}")
     element.assert_sibling("#line_item_date_#{@quote.line_item_dates.last.to_param}")
   end
 
   test 'Add new item before prev date' do
-    visit quote_path(@quote)
     create_new_item(DateTime.current + 1.day)
     create_new_item(DateTime.current)
     create_new_item(DateTime.current + 2.days)
