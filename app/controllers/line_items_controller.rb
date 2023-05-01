f6d8f5dbf9995d38796cfbc93f13105f89e8391c
lorrydriveloper <lorrydriveloper@gmail.com>
2023-05-01 10:08:45 +0100

    changes for turbo stream line_items

---
+++ app/controllers/line_items_controller.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+class LineItemsController < ApplicationController
+  before_action :set_quote
+  before_action :set_line_item_date
+  before_action :set_line_item, only: %i[edit update destroy]
+
+  def new
+    @line_item = @line_item_date.line_items.build
+  end
+
+  def edit; end
+
+  def create
+    @line_item = @line_item_date.line_items.build(line_item_params)
+
+    if @line_item.save
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: "Item #{t('created')}" }
+        format.turbo_stream { flash.now[:notice] = "Item #{t('created')}" }
+      end
+
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @line_item.update(line_item_params)
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: "Item #{t('updated')}" }
+        format.turbo_stream { flash.now[:notice] = "Item #{t('updated')}" }
+      end
+
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @line_item.destroy
+    respond_to do |format|
+      format.html { redirect_to quote_path(@quote), notice: "Item #{t('destroyed')}" }
+      format.turbo_stream { flash.now[:notice] = "Item #{t('destroyed')}" }
+    end
+  end
+
+  private
+
+  def line_item_params
+    params.require(:line_item).permit(:name, :description, :quantity, :unit_price)
+  end
+
+  def set_quote
+    @quote = current_company.quotes.find(params[:quote_id])
+  end
+
+  def set_line_item_date
+    @line_item_date = @quote.line_item_dates.find(params[:line_item_date_id])
+  end
+
+  def set_line_item
+    @line_item = @line_item_date.line_items.find(params[:id])
+  end
+end
